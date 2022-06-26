7a6e106ca62473fbadcac9c5691965d00162145e
switc <etiennebounias@orange.fr>
2022-06-26 20:03:52 +0200

    added values and debug mode

--- src/main.py
+++ src/main.py
@@ -11,16 +11,80 @@ dict={
             "str":"Bonjour",
             "encounter":None,
             "items":["Bonjour"],
-            "ritems":[]
+            "ritems":[],
+            "vchanges":[
+                {
+                    "name":"=",
+                    "operation":"=",
+                    "value":"10"
+
+                },
+                {
+                    "name":"+",
+                    "operation":"+",
+                    "value":"10"
+
+                },
+                {
+                    "name":"-",
+                    "operation":"-",
+                    "value":"10"
+
+                },
+                {
+                    "name":"*",
+                    "operation":"*",
+                    "value":"10"
+
+                },
+                {
+                    "name":"/",
+                    "operation":"/",
+                    "value":"10"
+
+                }
+            ]
         }
     ]
 
 },
             "items":["Bonjour"],
-            "ritems":["Auevoir"]
+            "ritems":["Auevoir"],
+            "vchanges":[
+                {
+                    "name":"=",
+                    "operation":"=",
+                    "value":"10"
+
+                },
+                {
+                    "name":"+",
+                    "operation":"+",
+                    "value":"10"
+
+                },
+                {
+                    "name":"-",
+                    "operation":"-",
+                    "value":"10"
+
+                },
+                {
+                    "name":"*",
+                    "operation":"*",
+                    "value":"10"
+
+                },
+                {
+                    "name":"/",
+                    "operation":"/",
+                    "value":"10"
+
+                }
+            ]
         }
     ]
 
 }
-story=util.Story(dict)
+story=util.Story(dict,True)
 story.start()
--- src/util.py
+++ src/util.py
@@ -1,5 +1,10 @@
 from os import system, name
-from time import sleep
+
+class bcolors:
+
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
 def clear():
    # for windows
    if name == 'nt':
@@ -10,9 +15,10 @@ def clear():
       _ = system('clear')
 
 class Story:
-    def __init__(self,encounterdict) -> None:
-        self.inventory = ["Aurevoir"]
+    def __init__(self,encounterdict,debug) -> None:
+        self.inventory = []
         self.value = {}
+        self.debug = debug
         self.encounter = encounter_from_dict(encounterdict,self)
     def render_inventory(self):
         print("Inventory")
@@ -20,27 +26,58 @@ class Story:
         for i in self.inventory:
             print(i)
         print("-------------------------------------------")
+        if self.debug == True:
+            print("Values")
+        print("-------------------------------------------")
+        
+        print(self.value)
+        print("-------------------------------------------")
     def start(self):
         self.encounter.Render()
          
 class Action:
-    def __init__(self,text: str,encounter,story : Story,items,ritems) -> None:
+    def __init__(self,text: str,encounter,story : Story,items,ritems,vchanges) -> None:
         self.text=text
         self.encounter=encounter
         self.items=items
         self.story=story
         self.ritems=ritems
+        self.vchanges=vchanges
     def Choose(self):
         if self.encounter==None:
             print('End')
         else:
-            clear()
+            if self.story.debug==False:
+                clear()
             for it in self.items:
                 self.story.inventory.append(it)
                         
             for rit in self.ritems:
                 if rit in self.story.inventory:
+                    
                     self.story.inventory.remove(rit)
+                else:
+                    if self.story.debug==True:
+                        print(bcolors.FAIL+"couldn't remove item " +rit+" item inexistent")
+                        print("there maybe a typo"+bcolors.ENDC)
+            for c in self.vchanges:
+                name = c["name"]
+                sign = c["operation"]
+                value= int(c["value"])
+                if name not in self.story.value.keys():
+                    self.story.value[name]=0
+                if sign == "=":
+                    self.story.value[name]=value
+                elif sign == "+":
+                    self.story.value[name]+=value
+                elif sign == "*":
+                    self.story.value[name]*=value
+                elif sign == "-":
+                    self.story.value[name]-=value
+                elif sign == "/":
+                    self.story.value[name]/=value
+
+            
             self.encounter.Render()
 
 class Encounter:
@@ -62,7 +99,7 @@ def encounter_from_dict(dict : dict,story : Story):
     if dict==None:
         return None
     actions=dict["actions"]
-    actions=[Action(a["str"],encounter_from_dict(a["encounter"],story),story,a["items"],a["ritems"]) for a in actions]
+    actions=[Action(a["str"],encounter_from_dict(a["encounter"],story),story,a["items"],a["ritems"],a["vchanges"]) for a in actions]
     return Encounter(description=dict["description"],actions=actions,story=story)
 
 
